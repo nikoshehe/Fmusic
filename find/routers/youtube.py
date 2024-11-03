b45013245c0124b9de04895ebfc35641dae01ab7
nikoshehe <162895356+nikoshehe@users.noreply.github.com>
2024-11-03 19:27:03 +0100

    Update youtube.py

--- find/routers/youtube.py
+++ find/routers/youtube.py
@@ -17,19 +17,26 @@ async def search_song(song: str):
 
 async def search_youtube(song_name: str):
     url = f"https://www.googleapis.com/youtube/v3/search?part=snippet&q={song_name}&key={YOUTUBE_API_KEY}"
-    response = requests.get(url)
-    
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+
     if response.status_code != 200:
-        raise HTTPException(status_code=response.status_code, detail = "Error with YT API")
-    
+        raise HTTPException(status_code=response.status_code, detail="Error with YT API")
+
     data = response.json()
+    print("API Response:", data)  # Výpis pro kontrolu obsahu
+
     results = []
 
     for item in data.get("items", []):
-        video_title = item["snippet"]["title"]
-        video_id = item["id"].get("videoId")
-        if video_id:
-            video_url = f"https://www.youtube.com/watch?v={video_id}"
-            results.append({"title": video_title, "url": video_url})
+        snippet = item.get("snippet")
+        if snippet:
+            video_title = snippet.get("title")  # Získání názvu
+            video_id = item["id"].get("videoId")
+            if video_title and video_id:
+                video_url = f"https://www.youtube.com/watch?v={video_id}"
+                results.append({"title": video_title, "url": video_url})
+        else:
+            print("Snippet not found in item:", item)
 
     return results
