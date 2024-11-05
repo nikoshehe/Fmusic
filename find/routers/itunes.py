d6ae35c0703dfa6fc6c72f9e5751137085ee8420
nikoshehe <162895356+nikoshehe@users.noreply.github.com>
2024-11-05 20:07:50 +0100

    Update itunes.py

--- find/routers/itunes.py
+++ find/routers/itunes.py
@@ -16,13 +16,13 @@ async def search_itunes(song: str):
         itunes_results = []
         for item in results:
             track_info = {
-                "song_name": item.track_name,  # Zkontroluj, zda to je správný atribut
-                "artist": item.artist_name,
-                "url": item.track_view_url,
+                "song_name": item.track_name if item.track_name else "Unknown Track",  # Ověřujeme, že track_name existuje
+                "artist": item.artist_name if item.artist_name else "Unknown Artist",  # Ověřujeme, že artist_name existuje
+                "url": item.track_view_url if item.track_view_url else "",  # Ověřujeme, že track_view_url existuje
             }
             itunes_results.append(track_info)
 
         return {"itunes_results": itunes_results}
-    
+
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error fetching data from iTunes: {str(e)}")
