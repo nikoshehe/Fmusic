3ab6feb7311b0d74d8d9b21aff71aadde928380b
nikoshehe <162895356+nikoshehe@users.noreply.github.com>
2024-11-05 18:53:45 +0100

    Update itunes.py

--- find/routers/itunes.py
+++ find/routers/itunes.py
@@ -11,18 +11,18 @@ async def search_itunes(song: str):
 
     url = f"https://itunes.apple.com/search?term={song}&media=music&limit=5"
     
-    
     async with httpx.AsyncClient() as client:
         response = await client.get(url)
 
-        if response.status_code != 200:
-            raise HTTPException(status_code=response.status_code, detail="Error fetching data from iTunes.")
-
+    if response.status_code != 200:
+        raise HTTPException(status_code=response.status_code, detail="Error fetching data from iTunes.")
+    
     # Parsování odpovědi
     data = response.json()
     results = []
-    
+
     for item in data.get("results", []):
+        # Zajištění, že klíče existují
         track_info = {
             "song_name": item.get("trackName", "Unknown Track"),
             "artist": item.get("artistName", "Unknown Artist"),
